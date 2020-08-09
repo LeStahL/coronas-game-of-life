94caa06c663c9c0188ec4054ace0520f7d293727
Alexander Kraus <nr4@z10.info>
2020-08-09 14:26:03 +0200

    added resource file to main menu

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -1,11 +1,25 @@
 #include "ui_mainmenu.h"
 #include "MainMenu.hpp"
 
+#include <QFile>
+#include <QTextStream>
+
 MainMenu::MainMenu(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MainMenu)
 {
     ui->setupUi(this);
+
+    QFile f(":/style/MainMenu.qss");
+    if (!f.exists())
+        printf("Style sheet for MainMenu is not present in resources.\n");
+    else
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        setStyleSheet(ts.readAll());
+        f.close();
+    }
 }
 
 MainMenu::~MainMenu()
